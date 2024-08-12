544b748056446ddb62a734b8fb9af463ffc8f02f
Susumu Yagi <susumu.yagi@gmail.com>
2024-08-12 16:37:47 +0900

    clean foxydownloader.py

--- foxydownloader.py
+++ foxydownloader.py
@@ -7,13 +7,9 @@ import appdirs
 import re
 import yaml
 import io
-# import codecs
 from getpass import getpass
 from simple_term_menu import TerminalMenu
 
-# yes this code looks weird but so is fox's komi notation 
-# which is giving a number without specifying the decimal point
-# seriously wtf?
 def komi_replacement(m):
     s = m.group()
     if s == "KM[375]":
@@ -93,70 +89,7 @@ def download_sgf(cid):
                 sys.exit(1)
     return sgf
 
-# def get_ogs_token(client_id, client_secret, username, password):
-#     values = { 
-#             "grant_type":"password",
-#             "username":username,
-#             "password":password,
-#             "client_id":client_id,
-#             "client_secret":client_secret,
-#             }
-#     url = "https://online-go.com/oauth2/token/"
-#     try:
-#         response = requests.post(url,data=values)
-#         response_json = response.json()
-#         return response_json['token_type'], response_json['access_token']
-#     except Exception as e:
-#         print(e)
-#         sys.exit(1)
-
-# def upload_to_ogs(filename, sgf, token_type, access_token):
-#     headers = {
-#             "Authorization":token_type+" "+access_token
-#             }
-#     files = {'file': (filename, sgf, 'application/x-go-sgf')}
-#     url = "https://online-go.com/api/v1/me/games/sgf/0"
-#     try:
-#         response = requests.post(url,headers=headers,files=files)
-#         if response.json()['success'] == "Files uploaded":
-#             return True
-#         else:
-#             return False
-#     except Exception as e:
-#         print(e)
-#         sys.exit(1)
-
-
 def main():
-    # no_ogs=False
-    # try:
-    #     ogs_option_file = io.open(appdirs.user_config_dir("foxydownloader")+'/config', 'r', encoding='utf8')
-    #     data_loaded = yaml.safe_load(ogs_option_file)
-    #     OGS_CLIENT_ID = data_loaded['ogs-client-id']
-    #     OGS_CLIENT_SECRET=data_loaded['ogs-client-secret']
-    # except Exception as e:
-    #     print(e)
-    #     print("OGS client data not set or set incorrectly. Please refer to the readme on how to configure this to upload to OGS.")
-    #     no_ogs = True
-
-    # ogs_upload = False
-    # if not(no_ogs):
-    #     ogs_access_token = ""
-    #     ogs_token_type = ""
-
-    #     print("What do you want to do with the downloaded Fox games?")
-    #     options = ["Save to disk","Upload to OGS"]
-    #     terminal_menu = TerminalMenu(options)
-    #     index = terminal_menu.show()
-
-    #     if index == None:
-    #         print("Aborting.")
-    #         sys.exit(0)
-    #     elif index == 1:
-    #         ogs_upload = True
-    #         ogs_token_type, ogs_access_token = get_ogs_token(OGS_CLIENT_ID, OGS_CLIENT_SECRET, \
-    #                 input("OGS username: "), getpass("OGS password: "))
-
     while True:
         username = input("Fox username: ").strip()
         if len(username) > 0:
@@ -205,12 +138,6 @@ def main():
                 print(f"Downloading {names[idx]} ...")
                 filename=f"{names[idx]}.sgf"
                 sgf = standardize_ranks(fix_komi(download_sgf(chessids[idx])))
-                # if ogs_upload:
-                #     upload_to_ogs(filename, sgf, ogs_token_type, ogs_access_token)
-                # else:
-                #     f = codecs.open(filename, 'w', 'utf-8')
-                #     f.write(sgf)
-                #     f.close()
                 with open(filename, 'w', encoding='utf-8') as f:
                     f.write(sgf)
             break
